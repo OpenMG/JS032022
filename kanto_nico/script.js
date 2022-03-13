e242ada7f8f1cf1dc66d87ca0de7873c08e9bdc9
kanto <mail@mail.com>
2022-03-13 22:02:39 +0300

    13-03-2022 22:00

--- kanto_nico/script.js
+++ kanto_nico/script.js
@@ -1,13 +1,19 @@
 let c= [34,34,-27,76,100,-46,27,39,-50,27,32]
 
-function ManalaDoublon(table){
+function ManalaDoublon(table, signe=true){
     let d=[]
     let bool=null
     for (let i = 0; i < c.length; i++) {
             bool=false
             for (let index=0; index<d.length; index++) {
-                if(Math.abs(d[index])=== Math.abs(c[i])){
-                    bool=true
+                if (signe) {
+                    if(Math.abs(d[index])=== Math.abs(c[i])){
+                        bool=true
+                    }
+                } else {
+                    if(d[index]=== c[i]){
+                        bool=true
+                    }
                 }
             }
             if(!bool){
@@ -15,4 +21,29 @@ function ManalaDoublon(table){
         }
     }
     return d
+}
+
+function somme(table,){
+    let neg=0
+    let pos=0
+    for (let i = 0; i < table.length; i++) {
+        if (table[i]<0) {
+            neg=neg+table[i]
+        } else {
+            pos= pos+ table[i]
+        }
+    }   
+    return [neg, pos]
+}
+
+console.table(ManalaDoublon(c,false))
+
+e=[23, 23, 23, 3, 4, 5, 5, 5, 1, 0, 3]
+
+function occurence(table) {
+    let temp= ManalaDoublon(table,false)
+    let occurence=[ ]
+    for (let i = 0; i < temp.length; i++) {
+        
+    }
 }
