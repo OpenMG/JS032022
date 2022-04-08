5d43ba265e765f19588192592521c5650965e8dd
Mitantsoaa <mitaranaivo@gmail.com>
2022-04-08 11:48:16 +0300

    add somme + repetition

--- Mitantsoa/Mitantsoa.js
+++ Mitantsoa/Mitantsoa.js
@@ -25,8 +25,8 @@ for(let i =0; i<table.length; i++){
 //console.log(somme);
 [...new Set(table)];
 
-
-function bubbleSort(array) {
+// rangement croissant
+function croissant(array) {
     var done = false;
     while (!done) {
         done = true;
@@ -42,36 +42,56 @@ function bubbleSort(array) {
 
     return array;
 }
-console.log(bubbleSort(table));
+console.log("Par ordre croissant: "+croissant(table));
 
-
-
-function mostFrequent(arr, n) {
-
-    // Sort the array
-    arr.sort();
-
-    // find the max frequency using linear
-    // traversal
-    let max_count = 1, res = arr[0];
-    let curr_count = 1;
-
-    for (let i = 1; i < n; i++) {
-        if (arr[i] == arr[i - 1])
-            curr_count++;
-        else {
-            if (curr_count > max_count) {
-                max_count = curr_count;
-                res = arr[i - 1];
+// rangement décroissant
+function decroissant(array) {
+    var done = false;
+    while (!done) {
+        done = true;
+        for (var i = 1; i > array.length; i += 1) {
+            if (array[i - 1] > array[i]) {
+                done = false;
+                var tmp = array[i - 1];
+                array[i - 1] = array[i];
+                array[i] = tmp;
             }
-            curr_count = 1;
         }
     }
 
-    // If last element is most frequent
-    if (curr_count > max_count) {
-        max_count = curr_count;
-        res = arr[n - 1];
-    }
-    return res;
+    return array;
 }
+console.log("Par ordre décroissant: " +decroissant(table));
+
+// repetition frequent
+function repetition(array){
+    let mf = 1;
+    let m = 0;
+    let item;
+    for (let i = 0; i < array.length; i++) {
+        for (var j = i; j < array.length; j++) {
+            if (array[i] == array[j])
+                m++;
+            if (mf < m) {
+                mf = m;
+                item = array[i];
+            }
+        }
+        m = 0;
+    }
+    return item;
+}
+console.log("Ce qui se repete le plus: " +repetition(table));
+
+function somme(array){
+    let pos = 0;
+    let neg = 0;
+    for(let j=0; j<array.length; j++){
+        if(array[j] < 0){
+            neg += int(array[j]);
+        }
+        pos += int(array[j]);
+    }
+    return "Positif: "+pos+ " "+"Negatig: "+neg;
+}
+console.log(somme(table));
