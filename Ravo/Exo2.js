07b9a0bb291e41f78b3e85e997cdb72d29372ab5
epramh <101051721+epramh@users.noreply.github.com>
2022-04-29 20:52:49 +0300

    Update function

---
+++ Ravo/Exo2.js
@@ -0,0 +1,11 @@
+//Trier par Ordre croissant
+let tab = [23, 0, -23, 1, 5, 0];
+tab.sort(function(a, b)
+{
+    return a - b
+});
+console.log(tab);
+
+//Trier par Ordre decroissant
+
+console.log(tab.reverse());
