39e94c80ebf6a4dc4feba0b3dfc7f2782883f84a
epramh <101051721+epramh@users.noreply.github.com>
2022-05-06 19:38:30 +0300

    Notes supérieur ou égale à 10 et Notes inférieur

--- Ravo/Exo.js
+++ Ravo/Exo.js
@@ -6,3 +6,25 @@ let Racine_Carrée = Math.sqrt(b);
 let Arrondi = Math.round(b);
 console.log(Racine_Carrée);
 console.log(Arrondi);
+
+//Affichage Notes
+let Notes = [7, 8, 13, 10, 9];
+let Sommes;
+for (let i = 0; i < Notes.length; i++)
+{
+    console.log(Notes[i]);
+}
+
+//Affichage Notes supérieur ou égale à 10 et Notes inférieur ou égale à 10
+for (let i = 0; i < Notes.length; i++)
+{
+    if (Notes [i] >= 10)
+     {
+         console.log('Notes supérieur ou égale à 10 : ' + Notes [i]);
+     }
+     else 
+     {
+         console.log(`Notes inférieur ou égale à 10 : ${Notes[i]}`);
+     }
+}
+
